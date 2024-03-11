6b9dcae249358ca8b15c13083c988c973901354c
Yeyobitz <diegoqm96@hotmail.com>
2024-03-11 10:46:28 -0300

    first commit

---
+++ clase 1.py
@@ -0,0 +1,51 @@
+##############################################
+# Clase 1
+# Diagnóstico e introducción a varios temas
+
+# Diseñar bien ya que sino hay que volver atrás y eso se llama RETROCESO
+# Diseñar bien es no tener que volver atrás
+# Si se diseña mal se pueden perder MILLONES por la hora de trabajo del programador
+# Diseño es todo aquello que se hace antes de programar (diseño de la base de datos, diseño de la interfaz, diseño de la lógica de negocio)
+# Diseño es pensar antes de hacer
+
+# QUÉ ES UN DATO?
+# El dato por si solo no tiene sentido
+# Pero suficiente dato se convierte en INFORMACIÓN
+# La información es el dato que tiene sentido
+
+# PARA QUÉ ES LA INFORMACIÓN?
+# Para tomar decisiones
+# La información es el insumo de las decisiones
+
+# Para eso estamos los informaticos, para transformar datos en información
+# A mayor cantidad de datos, mayor cantidad de información
+
+# un diagrama de clases se caracteriza por:
+# - Clase
+# - Atributos
+# - Métodos
+# - Relaciones
+
+# Clase: es el molde de los objetos
+# Atributos: son las características de los objetos
+# Métodos: son las acciones que pueden realizar los objetos
+# Relaciones: son las conexiones entre las clases
+
+# Un diagrama de secuencia se caracteriza por:
+# - Objetos
+# - Mensajes
+# - Líneas de vida
+
+# Objetos: son las instancias de las clases
+# Mensajes: son las acciones que se envían los objetos
+# Líneas de vida: son las líneas verticales que representan la vida de los objetos
+
+# Puede decirse que la teoría general de sistemas:
+# - Es una teoría que se aplica a cualquier sistema
+# - Se aplica a cualquier sistema
+# - saber porqué ocurren las cosas, como ocurren y que hacer para que ocurran
+
+# El modelamiento BPMN (Business Process Model and Notation) es un estándar para modelar procesos de negocio
+# Se utiliza como estandar internacional para modelar gráficamente los procesos de negocio
+
+# aaaaaaaa  
